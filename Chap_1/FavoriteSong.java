0c9412f30054b1aedb796e03e4187f5f8bb1be82
CharlesAlfredQuijoy-UCM <charlesquijoyy@gmail.com>
2025-09-16 08:41:15 +0800

    Add files via upload

---
+++ Chap_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong{
+   public static void main (String[]args){
+      System.out.println("Você vai ver quando eu passar de Mercedes");
+      System.out.println("Lembrando quando andava só de busão");
+      System.out.println("Você vai ver que o dinheiro não mudou");
+      System.out.println("Minha essência, minha alma, meu coração");
+   }
+
+}
